c2c67c298f202e1589a687e32a06f53b75e9020d
the-sage <ahmed187476@eng.zu.edu.eg>
2025-11-29 07:18:56 +0000

    Bump to version 0.0.2

--- consultations/consultations/doctype/consultation_session_booking/consultation_session_booking.py
+++ consultations/consultations/doctype/consultation_session_booking/consultation_session_booking.py
@@ -1,10 +1,41 @@
 # Copyright (c) 2025, elattar.systems and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-from frappe.website.website_generator import WebsiteGenerator
-
 
 class ConsultationSessionBooking(Document):
-	pass
+    def validate(self):
+        # Only auto-create/link customers for guest users submitting via web form
+        # Skip this logic when creating bookings from desk (logged-in users)
+        is_guest = frappe.session.user == "Guest"
+        
+        if is_guest and not self.client and self.client_name and self.client_email:
+            self.set_client()
+
+    def set_client(self):
+        # 1. Check if a customer exists with this email
+        existing_customer_name = frappe.db.get_value("Customer", 
+            {"email_id": self.client_email}, "name")
+
+        if existing_customer_name:
+            # Link existing customer
+            self.client = existing_customer_name
+        else:
+            # 2. Create a new customer
+            new_customer = frappe.get_doc({
+                "doctype": "Customer",
+                "customer_name": self.client_name,
+                "customer_type": "Individual",
+                "customer_group": "All Customer Groups", 
+                "territory": "All Territories",
+                "email_id": self.client_email,
+                "mobile_no": self.client_phone
+            })
+            
+            # Insert with ignore_permissions (Guests don't have permission to create Customers)
+            new_customer.flags.ignore_permissions = True
+            new_customer.insert()
+            
+            # 3. Link the new customer
+            self.client = new_customer.name
--- consultations/consultations/web_form/book_a_consultation/book_a_consultation.py
+++ consultations/consultations/web_form/book_a_consultation/book_a_consultation.py
@@ -1,5 +1,51 @@
 import frappe
 
 def get_context(context):
-	# do your magic here
-	pass
+    pass
+
+def validate(doc):
+    """
+    Runs on Web Form submission. 
+    1. Reads guest input from 'client_name', 'client_email', 'client_phone'.
+    2. Finds or creates a Customer.
+    3. Sets the mandatory 'client' link field to satisfy DocType validation.
+    """
+
+    # 1. Safety Check: If Client is already set (e.g. by internal staff), stop.
+    if doc.client:
+        return
+
+    # 2. READ DATA: Access the specific fields defined in your JSON
+    name_input = doc.client_name
+    email_input = doc.client_email
+    phone_input = doc.client_phone
+
+    # 3. VALIDATE: Ensure we have enough info to make a customer
+    if not name_input or not email_input:
+        frappe.throw("Name and Email are required to book a consultation.")
+
+    # 4. FIND CUSTOMER: Check if a customer exists with this email
+    existing_customer_name = frappe.db.get_value("Customer", 
+        {"email_id": email_input}, "name")
+
+    if existing_customer_name:
+        # Link existing customer
+        doc.client = existing_customer_name
+    else:
+        # 5. CREATE CUSTOMER: If not found, create a new one
+        new_customer = frappe.get_doc({
+            "doctype": "Customer",
+            "customer_name": name_input,
+            "customer_type": "Individual",
+            "customer_group": "All Customer Groups", 
+            "territory": "All Territories",
+            "email_id": email_input,
+            "mobile_no": phone_input
+        })
+        
+        # Insert with ignore_permissions (Guests don't have permission to create Customers)
+        new_customer.flags.ignore_permissions = True
+        new_customer.insert()
+        
+        # 6. LINK: Set the hidden 'client' field to the new Customer's name
+        doc.client = new_customer.name
